9815a1753a6d9779b4ee387c99441967ab87cabb
Ariel <Ariel Nuez>
2019-01-25 12:33:51 -0500

    Updated config with nginx + lua

--- configuration.nix
+++ configuration.nix
@@ -50,15 +50,17 @@
     libreoffice
   ];
 
-  networking.networkmanager.enable = true;
-  networking.nameservers = [ "8.8.8.8" ];
-  networking.hostName = "nuc";
-
-  networking.firewall.allowedTCPPortRanges = [
-  { from = 5432; to = 5432; }
-  { from = 9000; to = 9000; }
-  ];
-  networking.firewall.allowPing = true;
+  networking = {
+     hostName = "nuc";
+     networkmanager.enable = true;
+     nameservers = [ "1.1.1.1" "8.8.8.8"];
+     firewall = {
+        allowedTCPPortRanges = [
+           { from = 443; to = 444; }
+         ];
+        allowPing = true;
+     };
+  };
 
   time.timeZone = "America/Bogota";
 
@@ -76,22 +78,74 @@
   services.xserver.displayManager.sddm.autoLogin.enable = true;
   services.xserver.displayManager.sddm.autoLogin.user = "x";
   services.xserver.desktopManager.plasma5.enable = true;
-  services.sshd.enable = true;
+
+  security.pam.enableOTPW = true;
+
+  services.openssh = {
+    enable = true;
+    ports = [ 444 ];
+    gatewayPorts = "yes";
+    passwordAuthentication = false; 
+    permitRootLogin = "no";
+  };
 
   virtualisation.libvirtd.enable = true;
 
-  services.minio = {
-     enable = true;
-     accessKey = "opendata";
-     secretKey = "opendata";
-     dataDir = "/d/";
-     listenAddress = "0.0.0.0:9000";
-  };
- 
   users.users.x = {
     isNormalUser = true;
     home = "/x";
-    extraGroups = ["networkmanager" "libvirtd"];
+    extraGroups = ["libvirtd"];
+    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2SGK2vk4KGjkqUcDEdBYwHLj9utMTTShyPYAWBQx8jL0ezUoHqPl7ChJqLuI2ZWMVTW2QnGHl2oZjJRK6ngF0i9hhpjjONEHOdK9YHHaXeUXgad0mAT1R+365jIR1PYOvx9kC7pk8V1Iw3EHmnRRlAHtH19sAfGiyUopZ/N2gjVE0QMhotlKjwDlG9mQR/iFJq604R/nvAvTNXgHuuVou25t1kJkGNxAbiy3jOjKQHlR4NTTB2ttAtodJDU45+FNIWOiZYLbolYdAt9VLIYngDv9aSbxUCsF2ObHZ+Ovqmx0+BK1EKkZ01SYgwIQp3Nfk09xx03y28oFlvG+O6GX3 x@xs-MacBook.local" ];
+
   };
+
+ nixpkgs.config.packageOverrides = pkgs:
+ {
+  nginx = pkgs.nginx.override {
+    modules = [
+      pkgs.nginxModules.lua
+      pkgs.nginxModules.dav
+      pkgs.nginxModules.moreheaders
+    ];
+  };
+ };
+
+services.nginx = {
+  enable = true;
+  package = pkgs.nginx;
+  config = ''
+  events {
+    worker_connections  4096;
+  }
+  http {
+    include       ${pkgs.nginx}/conf/mime.types;
+    default_type  application/octet-stream;
+
+    server {
+      listen 443 ssl;
+      server_name *.puerti.co;
+      ssl_certificate /var/lib/acme/puerti.co/fullchain.pem;
+      ssl_certificate_key /var/lib/acme/puerti.co/key.pem;
+ 
+      root "/d/puerti.co";
+
+      location / {
+       default_type text/plain;
+       content_by_lua_block {
+         ngx.say('puerti.co is an experiment on geospatial data sharing')
+       }
+     }
+    }
+ }
+ '';
+
+};
+
+security.acme.certs = {
+  "puerti.co" = {
+    webroot = "/d/challenges";
+    email = "ingenieroariel@gmail.com";
+  };
+};
 
 }
