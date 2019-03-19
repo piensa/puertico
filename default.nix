30ee844c6a72562424ad5c5174c113e4a04afd2b
Ariel <ingenieroariel@gmail.com>
2019-03-19 11:45:26 -0500

    loaduninorte

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 
 let 
-  pkgs = import ( fetchTarball "https://github.com/NixOS/nixpkgs/archive/34beeb7d518bdfe4d77251e9391fb58e05f7d412.tar.gz") {};
+ # pkgs = import ( fetchTarball "https://github.com/NixOS/nixpkgs/archive/34beeb7d518bdfe4d77251e9391fb58e05f7d412.tar.gz") {};
+  pkgs = import <nixpkgs>{};
   piensa =  import (fetchTarball https://github.com/piensa/nur-packages/archive/1d0d8c3f9e19ac7fe9bf0eefa4419e6721736a9c.tar.gz) {};
   pg = pkgs.postgresql_11.withPackages(ps: [ps.postgis]);
   puertico-init = pkgs.writeShellScriptBin "puertico-init" ''
@@ -29,9 +30,14 @@ let
      url = https://download.geofabrik.de/south-america/colombia-190301.osm.pbf;
      sha256 = "1170pqz2bhfq2msdylf9i1z53d1gyshipd4h6zf2i9wyxb7gz3l0";
   };
-  puertico-loadcolombia = pkgs.writeShellScriptBin "puertico-loadcolombia" ''
-    echo ${piensa.colombia}
-    imposm import -connection postgis://puertico:puertico@localhost/puertico -mapping ${piensa.puertico-osm}/imposm3.json -read ${colombia} -write -overwritecache
+  puertico-createuninorte = pkgs.writeShellScriptBin "puertico-createuninorte" ''
+    osmconvert ${colombia} -B=uninorte.poly  -o=uninorte.pbf
+  '';
+  puertico-convertuninorte = pkgs.writeShellScriptBin "puertico-convertuninorte" ''
+    osmconvert uninorte.osm -B=uninorte.poly  -o=uninorte.pbf
+  '';
+  puertico-loaduninorte = pkgs.writeShellScriptBin "puertico-loaduninorte" ''
+    imposm import -connection postgis://puertico:puertico@localhost/puertico -mapping ${piensa.puertico-osm}/imposm3.json -read uninorte.pbf -write -overwritecache
     imposm  import -connection postgis://puertico:puertico@localhost/puertico -mapping ${piensa.puertico-osm}/imposm3.json -deployproduction 
     psql puertico -a -f  ${piensa.puertico-osm}/postgis_helpers.sql
     psql puertico -a -f  ${piensa.puertico-osm}/postgis_index.sql
@@ -42,7 +48,7 @@ in pkgs.stdenv.mkDerivation rec {
    src = builtins.filterSource (p: t: pkgs.lib.cleanSourceFilter p t && baseNameOf p != "data") ./.;
 
    buildInputs = with pkgs; [
-   #  pg
+     pg
      minio mc
      curl unzip gdal less
      osmctools
@@ -59,7 +65,8 @@ in pkgs.stdenv.mkDerivation rec {
     puertico-tegola
     puertico-stop
     puertico-loadworld
-    puertico-loadcolombia
+    puertico-loaduninorte
+    puertico-createuninorte
    ];
   shellHooks = ''
      export PGDATA=$PWD/data;
