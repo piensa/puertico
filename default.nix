2092ccd0ae1f46dc118b2d02bd9a89ffb22c114d
Your Name <you@example.com>
2019-03-26 23:28:30 -0500

    Switched to local imposm3 in case changes are needed

--- default.nix
+++ default.nix
@@ -37,8 +37,8 @@ let
     osmconvert uninorte.osm -B=uninorte.poly  -o=uninorte.pbf
   '';
   puertico-loaduninorte = pkgs.writeShellScriptBin "puertico-loaduninorte" ''
-    imposm import -connection postgis://puertico:puertico@localhost/puertico -mapping ${piensa.puertico-osm}/imposm3.json -read uninorte.pbf -write -overwritecache -srid 4326
-    imposm  import -connection postgis://puertico:puertico@localhost/puertico -mapping ${piensa.puertico-osm}/imposm3.json -deployproduction -srid 4326
+    imposm import -connection postgis://puertico:puertico@localhost/puertico -mapping imposm3.json -read uninorte.pbf -write -overwritecache -srid 4326
+    imposm  import -connection postgis://puertico:puertico@localhost/puertico -mapping imposm3.json -deployproduction -srid 4326
     psql puertico -a -f  ${piensa.puertico-osm}/postgis_helpers.sql
 #    psql puertico -a -f  ${piensa.puertico-osm}/postgis_index.sql
   '';
