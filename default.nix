1c876609d6fd8aed85e851cec976621ac765b098
Your Name <you@example.com>
2019-03-29 14:26:08 -0500

    Move all data to a folder called var

--- default.nix
+++ default.nix
@@ -4,18 +4,32 @@ let
   pkgs = import <nixpkgs>{};
   piensa =  import (fetchTarball https://github.com/piensa/nur-packages/archive/1d0d8c3f9e19ac7fe9bf0eefa4419e6721736a9c.tar.gz) {};
   pg = pkgs.postgresql_11.withPackages(ps: [ps.postgis]);
-  nginx-config = pkgs.writeText "nginx.conf" ''
-daemon            off;
-worker_processes  2;
 
-events {
+  area-poly = pkgs.writeText "area.poly" ''
+salida-0
+1
+        -74.85208511352539      11.027219456385142
+        -74.85551834106445      11.025955764462422
+        -74.8550033569336       11.01584603362195
+        -74.84298706054688      11.011886294383482
+        -74.83989715576172      11.015677535164972
+        -74.8399829864502       11.020648199089703
+        -74.85208511352539      11.027219456385142
+END
+END
+  '';
+  nginx-config = pkgs.writeText "nginx.conf" ''
+  daemon            off;
+  worker_processes  2;
+
+  events {
     use           epoll;
     worker_connections  128;
-}
+  }
 
-error_log         error.log info;
+  error_log         error.log info;
 
-http {
+  http {
     server_tokens off;
     charset       utf-8;
 
@@ -30,10 +44,8 @@ http {
         location      / {
             root      /x/puertico/static;
         }
-
     }
-
-}
+  }
   '';
   puertico-init = pkgs.writeShellScriptBin "puertico-init" ''
     initdb -D $PGDATA --no-locale --encoding=UTF8 --auth=trust --auth-host=trust 
@@ -45,7 +57,7 @@ http {
     psql puertico -c "CREATE EXTENSION hstore;"
   '';
   puertico-nginx = pkgs.writeShellScriptBin "puertico-nginx" ''
-    nginx -c ${nginx-config} -p /x/puertico/state
+    nginx -c ${nginx-config} -p /x/puertico/var
   '';
   puertico-start = pkgs.writeShellScriptBin "puertico-start" ''
     pg_ctl -D $PGDATA -l $PGDATA/server.log start -w
@@ -54,7 +66,7 @@ http {
     tegola serve --config=conf/tegola.toml
   '';
   puertico-cache = pkgs.writeShellScriptBin "puertico-cache" ''
-    tegola cache seed --config=conf/tegola.toml --bounds "-74.855518, 11.011886, -74.839897, 11.027220" --min-zoom 17 --max-zoom 22 --overwrite
+    tegola cache seed --config=conf/tegola.toml --bounds "-74.855518, 11.011886, -74.839897, 11.027220" --min-zoom 17 --max-zoom 20 --overwrite
   '';
   puertico-stop = pkgs.writeShellScriptBin "puertico-stop" ''
     pg_ctl stop
@@ -63,26 +75,23 @@ http {
    sh ${piensa.puertico-osm}/natural_earth.sh
    sh ${piensa.puertico-osm}/osm_land.sh
   '';
-  colombia = pkgs.fetchurl {
+  country-osm = pkgs.fetchurl {
      url = https://download.geofabrik.de/south-america/colombia-190301.osm.pbf;
      sha256 = "1170pqz2bhfq2msdylf9i1z53d1gyshipd4h6zf2i9wyxb7gz3l0";
   };
-  puertico-createuninorte = pkgs.writeShellScriptBin "puertico-createuninorte" ''
-    osmconvert ${colombia} -B=uninorte.poly  -o=uninorte.pbf
+  puertico-createarea = pkgs.writeShellScriptBin "puertico-createarea" ''
+    osmconvert ${country-osm} -B=${area-poly}  -o=area.pbf
   '';
-  puertico-convertuninorte = pkgs.writeShellScriptBin "puertico-convertuninorte" ''
-    osmconvert uninorte.osm -B=uninorte.poly  -o=uninorte.pbf
-  '';
-  puertico-loaduninorte = pkgs.writeShellScriptBin "puertico-loaduninorte" ''
-    imposm import -connection postgis://puertico:puertico@localhost/puertico -mapping conf/imposm3.json -read uninorte.pbf -write -overwritecache -srid 4326
+  puertico-loadarea = pkgs.writeShellScriptBin "puertico-loadarea" ''
+    imposm import -connection postgis://puertico:puertico@localhost/puertico -mapping conf/imposm3.json -read area.pbf -write -overwritecache -srid 4326
     imposm  import -connection postgis://puertico:puertico@localhost/puertico -mapping conf/imposm3.json -deployproduction -srid 4326
     psql puertico -a -f  ${piensa.puertico-osm}/postgis_helpers.sql
 #    psql puertico -a -f  ${piensa.puertico-osm}/postgis_index.sql
   '';
 in pkgs.stdenv.mkDerivation rec {
-   name = "hola";
+   name = "puertico";
 
-   src = builtins.filterSource (p: t: pkgs.lib.cleanSourceFilter p t && baseNameOf p != "data") ./.;
+   src = builtins.filterSource (p: t: pkgs.lib.cleanSourceFilter p t && baseNameOf p != "state") ./.;
 
    buildInputs = with pkgs; [
      pg
@@ -103,11 +112,11 @@ in pkgs.stdenv.mkDerivation rec {
     puertico-cache
     puertico-stop
     puertico-loadworld
-    puertico-loaduninorte
-    puertico-createuninorte
+    puertico-loadarea
+    puertico-createarea
    ];
   shellHooks = ''
-     mkdir -p state/logs
-     export PGDATA=$PWD/state/data;
+     mkdir -p var/logs
+     export PGDATA=$PWD/var/data;
   '';
 }
